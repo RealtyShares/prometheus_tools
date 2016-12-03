23634113529ec146a6ca31fa6c8441a31a0abab9
Michael Liu <mike@ip-10-15-5-54.us-west-2.compute.internal>
2016-12-02 16:07:52 -0800

    nginx exporter

--- nginx_exporter_handle_https_redirect/nginx_exporter.go
+++ nginx_exporter_handle_https_redirect/nginx_exporter.go
@@ -77,13 +77,11 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 
 func (e *Exporter) collect(ch chan<- prometheus.Metric) error {
 
-	// client := &http.Client{}
-	// req, err := http.NewRequest("GET", e.URI, nil)
-	// req.Header.Set("X-Forwarded-Proto", "https")
-	// resp, err := client.Do(req)
-	e.Header().Set("X-Forwarded-Proto", "https")
-
-	resp, err := e.client.Get(e.URI)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", e.URI, nil)
+	req.Header.Set("X-Forwarded-Proto", "https")
+	resp, err := client.Do(req)
+	// resp, err := e.client.Get(e.URI)
 	if err != nil {
 		return fmt.Errorf("Error scraping nginx: %v", err)
 	}
